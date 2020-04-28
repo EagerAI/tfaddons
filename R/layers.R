a0367d66625bd405bcfae029844dad30c5b07387
Turgut Abdullayev <turqut.a.314@gmail.com>
2020-04-28 15:39:28 +0400

    Keras-based MultiHead Attention Layer

--- R/layers.R
+++ R/layers.R
@@ -69,3 +69,79 @@ layer_normalization <- function(object,
 
 
 
+#' @title Keras-based multi head attention layer
+#'
+#' @description MultiHead Attention layer.
+#'
+#' @details Defines the MultiHead Attention operation as defined in
+#' [Attention Is All You Need](https://arxiv.org/abs/1706.03762) which takes
+#' in a `query`, `key` and `value` tensors returns the dot-product attention
+#' between them:
+#' ```
+#' mha = layer_multi_head_attention(head_size=128, num_heads=128)
+#' query = tf$random$uniform(list(32L, 20L, 200L)) # (batch_size, query_elements, query_depth)
+#' key = tf$random$uniform(list(32L, 15L, 300L)) # (batch_size, key_elements, key_depth)
+#' value = tf$random$uniform(list(32L, 15L, 400L)) # (batch_size, key_elements, value_depth)
+#' attention = mha(list(query, key, value)) # (batch_size, query_elements, value_depth)
+#' ```
+#'
+#' If `value` is not given then internally `value = key` will be used:
+#' ```
+#' mha = layer_multi_head_attention(head_size=128, num_heads=128)
+#' query = tf$random$uniform(list(32L, 20L, 200L)) # (batch_size, query_elements, query_depth)
+#' key = tf$random$uniform(list(32L, 15L, 300L)) # (batch_size, key_elements, key_depth)
+#' attention = mha(list(query, key)) # (batch_size, query_elements, value_depth)
+#' ```
+#'
+#' @param object Model or layer object
+#' @param head_size int, dimensionality of the `query`, `key` and `value` tensors after the linear transformation.
+#' @param num_heads int, number of attention heads.
+#' @param output_size int, dimensionality of the output space, if `NULL` then the input dimension of `value` or `key` will be used, default `NULL`.
+#' @param dropout float, `rate` parameter for the dropout layer that is applied to attention after softmax, default `0`.
+#' @param use_projection_bias bool, whether to use a bias term after the linear output projection.
+#' @param return_attn_coef bool, if `TRUE`, return the attention coefficients as an additional output argument.
+#' @param kernel_initializer initializer, initializer for the kernel weights.
+#' @param kernel_regularizer regularizer, regularizer for the kernel weights.
+#' @param kernel_constraint constraint, constraint for the kernel weights.
+#' @param bias_initializer initializer, initializer for the bias weights.
+#' @param bias_regularizer regularizer, regularizer for the bias weights.
+#' @param bias_constraint constraint, constraint for the bias weights.
+#' @param ... additional parameters to pass
+#' @importFrom keras create_layer
+#'
+#' @export
+layer_multi_head_attention <- function(object, head_size, num_heads, output_size = NULL, dropout = 0.0,
+                                       use_projection_bias = TRUE, return_attn_coef = FALSE,
+                                       kernel_initializer = "glorot_uniform", kernel_regularizer = NULL,
+                                       kernel_constraint = NULL, bias_initializer = "zeros",
+                                       bias_regularizer = NULL, bias_constraint = NULL, ...) {
+
+  args = list(
+    head_size = as.integer(head_size),
+    num_heads = as.integer(num_heads),
+    output_size = output_size,
+    dropout = dropout,
+    use_projection_bias = use_projection_bias,
+    return_attn_coef = return_attn_coef,
+    kernel_initializer = kernel_initializer,
+    kernel_regularizer = kernel_regularizer,
+    kernel_constraint = kernel_constraint,
+    bias_initializer = bias_initializer,
+    bias_regularizer = bias_regularizer,
+    bias_constraint = bias_constraint,
+    ...
+  )
+
+  if(!is.null(output_size)) {
+    args$output_size <- as.integer(output_size)
+  }
+
+  create_layer(tfa$layers$MultiHeadAttention, object, args)
+
+}
+
+
+
+
+
+
