0338af07051fb1a053ba7453a77a1db2f312425f
Turgut Abdullayev <turqut.a.314@gmail.com>
2020-05-25 14:46:10 +0400

    new version of addons 0.10.0

--- R/install.R
+++ R/install.R
@@ -2,7 +2,7 @@
 #'
 #' @description This function is used to install the `TensorFlow SIG Addons` python module
 
-#' @param version for specific version of `TensorFlow SIG Addons`, e.g. "0.9.1"
+#' @param version for specific version of `TensorFlow SIG Addons`, e.g. "0.10.0"
 #' @param ... other arguments passed to [reticulate::py_install()].
 #' @param restart_session Restart R session after installing (note this will only occur within RStudio).
 #' @return a python module `tensorflow_addons`
@@ -11,7 +11,7 @@
 install_tfaddons <- function(version = NULL, ..., restart_session = TRUE) {
 
   if (is.null(version))
-    module_string <- paste0("tensorflow-addons==", '0.9.1')
+    module_string <- paste0("tensorflow-addons==", '0.10.0')
   else
     module_string <- paste0("tensorflow-addons==", version)
 
